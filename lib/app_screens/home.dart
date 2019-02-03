0cbde68f20b1d31cb5b9b64cf81f1edebb027dd0
Ezlan <eizs798@pop-os.localdomain>
2019-02-03 10:07:25 +0800

    Feat | Columns and Rows

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -6,26 +6,187 @@ class Home extends StatelessWidget {
     // TODO: implement build
     return Center(
         child: Container(
+        padding: EdgeInsets.only(left: 30.0, right: 30.0),
         alignment: Alignment.center,
         color: Colors.red,
         // width: 200.0,
         // height: 100.0,
         // margin: EdgeInsets.only(bottom: 15.0, top: 15.0),
         // margin: EdgeInsets.all(15.0),
-        child: Text(
-          "SUPREME",
-          textDirection: TextDirection.ltr,
-            style: TextStyle(
-              decoration: TextDecoration.none,
-              fontSize: 75.0,
-              fontFamily: 'Raleway',
-              fontWeight: FontWeight.w700,
-              fontStyle: FontStyle.italic,
-              color: Colors.white
-            ),
-          ),
+        child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(child: Text(
+                  "SUPREME",
+                  textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 50.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white
+                    ),
+                  )), 
+                  Expanded(child:Text(
+                  "LEGIT",
+                  textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 50.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white
+                    ),
+                  )),  
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(child: Text(
+                  "SUPREME",
+                  textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 50.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white
+                    ),
+                  )), 
+                  Expanded(child:Text(
+                  "LEGIT",
+                  textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 50.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white
+                    ),
+                  )),  
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(child: Text(
+                  "SUPREME",
+                  textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 50.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white
+                    ),
+                  )), 
+                  Expanded(child:Text(
+                  "LEGIT",
+                  textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 50.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white
+                    ),
+                  )),  
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(child: Text(
+                  "SUPREME",
+                  textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 50.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white
+                    ),
+                  )), 
+                  Expanded(child:Text(
+                  "LEGIT",
+                  textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 50.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white
+                    ),
+                  )),  
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(child: Text(
+                  "SUPREME",
+                  textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 50.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white
+                    ),
+                  )), 
+                  Expanded(child:Text(
+                  "LEGIT",
+                  textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      fontSize: 50.0,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.white
+                    ),
+                  )),  
+                ],
+              )
+            ],
+        )
+        
+        // Row(
+        //   children: <Widget>[
+        //     Text(
+        //     "SUPREME",
+        //     textDirection: TextDirection.ltr,
+        //       style: TextStyle(
+        //         decoration: TextDecoration.none,
+        //         fontSize: 50.0,
+        //         fontFamily: 'Raleway',
+        //         fontWeight: FontWeight.w700,
+        //         fontStyle: FontStyle.italic,
+        //         color: Colors.white
+        //       ),
+        //     ), 
+        //     Expanded(child:Text(
+        //     "LEGIT",
+        //     textDirection: TextDirection.ltr,
+        //       style: TextStyle(
+        //         decoration: TextDecoration.none,
+        //         fontSize: 50.0,
+        //         fontFamily: 'Raleway',
+        //         fontWeight: FontWeight.w700,
+        //         fontStyle: FontStyle.italic,
+        //         color: Colors.white
+        //       ),
+        //     )),  
+        //   ],
+        // )                
       ),
     );
   }
-
 }

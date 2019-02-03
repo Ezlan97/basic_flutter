e0c7454ff4100eebfb996668f74c896dd5c4fd8e
Ezlan <eizs798@pop-os.localdomain>
2019-02-03 14:53:25 +0800

    Feat | long list view

--- lib/main.dart
+++ lib/main.dart
@@ -6,29 +6,53 @@ void main() {
     debugShowCheckedModeBanner: false,
     title: "exploring UI Widget",
     home: Scaffold(
-      appBar: AppBar(title: Text('Basic List View'),),
+      appBar: AppBar(title: Text('long list'),),
       body: getListView(),
     ),
   ));
 }
 
-Widget getListView() {
-  var listView = ListView(
-    children: <Widget>[
+// Widget getListView() {
+//   var listView = ListView(
+//     children: <Widget>[
 
-      ListTile(
-        leading: Icon(Icons.landscape),
-        title: Text('Landscape'),
-        subtitle: Text('Beautiful View!'),
-        trailing: Icon(Icons.wb_sunny),
-      ),
-      ListTile(
-        leading: Icon(Icons.laptop),
-        title: Text('Linux Os'),
-        onTap: () => debugPrint('laptop Tapped!'),
-      ),
-    ],
+//       ListTile(
+//         leading: Icon(Icons.landscape),
+//         title: Text('Landscape'),
+//         subtitle: Text('Beautiful View!'),
+//         trailing: Icon(Icons.wb_sunny),
+//       ),
+//       ListTile(
+//         leading: Icon(Icons.laptop),
+//         title: Text('Linux Os'),
+//         onTap: () => debugPrint('laptop Tapped!'),
+//       ),
+//     ],
+//   );
+
+//   return listView;
+// }
+
+List<String> getListElements() {
+
+  var items = List<String>.generate(1000, (counter) => "item $counter");
+  return items;
+}
+
+Widget getListView() {
+  var listItems = getListElements();
+  
+  var listViews = ListView.builder(
+    itemBuilder: (context, index) {
+      return ListTile(
+        title: Text(listItems[index]),
+        leading: Icon(Icons.ac_unit),
+        onTap: () {
+          debugPrint('${listItems[index]} was tapped');
+        },
+      );
+    }
   );
 
-  return listView;
+  return listViews;
 }

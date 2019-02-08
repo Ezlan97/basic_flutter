19a6422687b0cb03be94dce6b68661335d834dd8
eizlan <eizs798@pop-os.localdomain>
2019-02-08 22:39:46 +0800

    Feat | stateful widget example

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,59 @@
 import 'package:flutter/material.dart';
-import './app_screens/main_screen.dart';
 
-void main() => runApp(new MyFlutterApp());
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Stateful App Example',
+      home: FavoriteCity(),
+    )
+  );
+}
 
-class MyFlutterApp extends StatelessWidget {
+class FavoriteCity extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _FavoriteCityState();
+  }
+}
 
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "My Flutter App",
-        home: Scaffold(
-            appBar: AppBar(title: Text("My First App Screen"),),
-            body: MainScreen()
-      )
+class _FavoriteCityState extends State<FavoriteCity> {
+
+  String nameCity = '';
+
+  @override
+  Widget build(BuildContext context) {
+
+    debugPrint("Favorite City Widget Is Created! ");
+
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stateful App Example'),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(30.0),
+        child: Column(
+          children: <Widget>[
+            TextField(        
+              onSubmitted: (String userInput) {
+                setState(() {
+                  debugPrint("Set state is called, this tell the framework ro redraw the favcity widget");
+                  nameCity = userInput;
+                });                
+              },
+            ),
+            Padding (
+              padding: EdgeInsets.all(30.0),
+              child: Text(
+                'Your best city is $nameCity',
+                style: TextStyle(fontSize: 20.0),
+              )
+            )
+          ],
+        ),
+      ),
     );
   }
 }
